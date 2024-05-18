0bc5c021b8a95025bf2ed9e20b17402f73c3eb1e
Admin <shnartho@gmail.com>
2024-05-18 11:36:52 +0000

    fixed: title

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -30,7 +30,7 @@ const App: FC<AppPropsWithLayout> = (props: AppPropsWithLayout) => {
     <CacheProvider value={emotionCache}>
       <Head>
         <meta name="viewport" content="initial-scale=1, width=device-width" />
-        <title>metasurfai university</title>
+        <title>Metasurfai University</title>
       </Head>
       <MUIProvider>
         {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
